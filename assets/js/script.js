a0f5be0234cf90ae6f6a5ef6d90db6258cc75332
In Code We Trust <njderenne@gmail.com>
2020-09-21 14:23:36 -0500

    changed form submit type

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
-var buttonEI = document.querySelector("#save-task");
+var formEI = document.querySelector("#task-form");
 var tasksToDoEI = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+
     var listItemEI = document.createElement("li");
     listItemEI.className = "task-item";
     listItemEI.textContent = "This is a new task.";
     tasksToDoEI.appendChild(listItemEI);
-}
+};
 
-buttonEI.addEventListener("click", createTaskHandler);
+formEI.addEventListener("submit", createTaskHandler);
